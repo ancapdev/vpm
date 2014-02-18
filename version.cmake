b3bce111ced6a2ea777d66387e060840aae1a031
Christian Rorvik <christian.rorvik@mavensecurities.com>
2014-02-18 18:33:54 +0000

    update version

--- version.cmake
+++ version.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) 2011-2014, Christian Rorvik
 # Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
 
-set(VPM_FRAMEWORK_VERSION 1.06.00)
+set(VPM_FRAMEWORK_VERSION 1.06.01)
 
 macro(vpm_set_version _name _version)
   if(VPM_VERBOSE)
